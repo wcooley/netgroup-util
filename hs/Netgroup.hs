0e74e20c9e1ec79bdac5f307afd95a7ed112dbed
Wil Cooley <wcooley@pdx.edu>
2011-03-05 15:32:06 -0800

    Implemented 'show' for Netgroup

--- hs/Netgroup.hs
+++ hs/Netgroup.hs
@@ -9,6 +9,8 @@ module Netgroup
 , inNetgroup
 ) where
 
+import Data.Maybe (fromMaybe)
+
 type NetgroupTriple  = String
 
 data Netgroup = Netgroup { netgroup         :: String
@@ -16,10 +18,26 @@ data Netgroup = Netgroup { netgroup         :: String
                          , netgroupTriples  :: [NetgroupTriple]
                          , memberNetgroups  :: [Netgroup]
                          }
-                        deriving (Eq, Show)
+                        --deriving (Show, Eq)
+                        deriving (Eq)
 
 instance Ord Netgroup where
         compare ng1 ng2 = compare (netgroup ng1) (netgroup ng2)
+
+
+instance Show Netgroup where
+    show ng =    "\nNetgroup { netgroup=" ++ (quot (netgroup ng)) ++ ",\n"
+              ++ "    description=\"" ++ (fromMaybe "none" (description ng)) ++ "\",\n"
+              ++ "    triples=[" ++ (fmt_join (netgroupTriples ng)) ++ "],\n"
+              ++ "    members=[" ++ (unlines (map netgroup (memberNetgroups ng))) ++ "]\n"
+              ++ "}\n"
+        where   fmt_join [] = ""
+                fmt_join xs = foldl1 (\x y -> (quot x) ++ sep
+                                           ++ (pad 13 " ") ++ (quot y)) xs
+                pad n c = concat(replicate n c)
+                quot x = "\"" ++ x ++ "\""
+                sep = ",\n"
+
 
 isFlatNetgroup :: Netgroup -> Bool
 isFlatNetgroup ng = ( memberNetgroups ng ) == []
