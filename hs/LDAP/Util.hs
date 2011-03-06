7940c724d0b04f319b7ac1bdd6cdeb1c471513a1
Wil Cooley <wcooley@pdx.edu>
2011-03-05 23:14:22 -0800

    Added several ldapAttr variations & tests

--- hs/LDAP/Util.hs
+++ hs/LDAP/Util.hs
@@ -4,9 +4,11 @@
 
 module LDAP.Util where
 
+import Data.Maybe (fromMaybe)
 import LDAP
 
 
+-- Pick out a list values for an attribute in an LDAPEntry
 ldapAttr :: String -> LDAPEntry -> Maybe [String]
 ldapAttr attr entry =
             case filter_attr of
@@ -15,3 +17,18 @@ ldapAttr attr entry =
             where filter_attr =
                     filter (\x -> fst x == attr)
                         (leattrs entry)
+
+-- Same, but only the first of a multi-valued attribute
+ldapAttr1 :: String -> LDAPEntry -> Maybe String
+ldapAttr1 attr entry = fmap head (ldapAttr attr entry)
+
+-- Likewise, but take it out of the Maybe ([] for Nothing)
+ldapAttrYeah :: String -> LDAPEntry -> [String]
+ldapAttrYeah attr entry = fromMaybe [] $ ldapAttr attr entry
+
+-- Likewise, but "" for Nothing
+ldapAttr1Yeah :: String -> LDAPEntry -> String
+ldapAttr1Yeah attr entry =  case res1 of
+                                [] -> ""
+                                otherwise -> head res1
+            where res1 = ldapAttrYeah attr entry
--- hs/t_LDAP_Util.hs
+++ hs/t_LDAP_Util.hs
@@ -59,12 +59,41 @@ test_ldap_results = [
     ]
 
 ldapattr_tests =  TestLabel "Tests of ldapAttr* functions" $ TestList [
-        TestCase (assertEqual "single-valued attr"
+        TestCase (assertEqual "ldapAttr: single-valued"
+                        (Just ["mail"])
+                        ( ldapAttr "cn" $ test_ldap_results !! 0 )),
+        TestCase (assertEqual "ldapAttr: multi-valued"
+                        (Just ["mgmt", "mon"])
+                        ( ldapAttr "memberNisNetgroup" $
+                                    test_ldap_results !! 5 )),
+        TestCase (assertEqual "ldapAttrYeah: single-valued"
                         ["mail"]
-                        ( fromJust $ ldapAttr "cn" $ test_ldap_results !! 0 )),
-        TestCase (assertEqual "multi-valued attr"
+                        ( ldapAttrYeah "cn" $ test_ldap_results !! 0 )),
+        TestCase (assertEqual "ldapAttrYeah: multi-valued"
                         ["mgmt", "mon"]
-                        ( fromJust $ ldapAttr "memberNisNetgroup" $ test_ldap_results !! 5 ))
+                        ( ldapAttrYeah "memberNisNetgroup" $
+                                    test_ldap_results !! 5 )),
+        TestCase (assertEqual "ldapAttr1: multi-valued"
+                        (Just "mgmt")
+                        ( ldapAttr1 "memberNisNetgroup" $
+                                    test_ldap_results !! 5 )),
+        TestCase (assertEqual "ldapAttr1Yeah: multi-valued"
+                        "mgmt"
+                        ( ldapAttr1Yeah "memberNisNetgroup" $
+                                    test_ldap_results !! 5 )),
+        TestCase (assertEqual "ldapAttr, non-existent attr"
+                        Nothing
+                        ( ldapAttr "nonExistentAttr" $
+                                    test_ldap_results !! 5 )),
+        TestCase (assertEqual "ldapAttrYeah, non-existent attr"
+                        []
+                        ( ldapAttrYeah "nonExistentAttr" $
+                                    test_ldap_results !! 5 )),
+        TestCase (assertEqual "ldapAttr1Yeah, non-existent attr"
+                        ""
+                        ( ldapAttr1Yeah "nonExistentAttr" $
+                                    test_ldap_results !! 5 ))
+
     ]
 
 tests = TestList [ ldapattr_tests ]
