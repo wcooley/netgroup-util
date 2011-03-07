aa5f3b788458eb6ec9b561b41a5017e75b32e37d
Wil Cooley <wcooley@pdx.edu>
2011-03-06 18:02:53 -0800

    Added a number of functions in an attempt to figure out how to teal with IO monad that results of ldapSearch are contained within

--- hs/LDAP/Netgroup.hs
+++ hs/LDAP/Netgroup.hs
@@ -34,6 +34,13 @@ searchNetgroupsInLDAP = do
                     results <- search_ldap_netgroup lconn
                     return results
 
+-- netgroupFromLDAP :: IO (Map.Map String LDAPEntry) -> IO Netgroup
+netgroupFromLDAP ngmap entry =
+        Netgroup {  netgroup        = netgroup_name entry,
+                    description     = netgroup_description entry,
+                    netgroupTriples = netgroup_triples entry,
+                    memberNetgroups = netgroup_members entry ngmap
+                 }
 
 -- Attribute extractors to go along with Netgroup records
 -- "cn" can be multivalued but should always exist
@@ -44,15 +51,39 @@ netgroup_name      = ldapAttr1Yeah "cn"
 netgroup_description = ldapAttr1 "description"
 netgroup_triples = ldapAttrYeah "nisNetgroupTriple"
 
-netgroup_members entry = []
-netgroup_members_ entry = ldapAttr "memberNisNetgroup"
+netgroup_members_ entry ngmap = mem_entries
+        where   mem_names = ldapAttrYeah "memberNisNetgroup" entry
+                mem_entries = map ( \n -> netgroup_from_map n ngmap ) mem_names
 
-netgroupFromLDAP entry =
-        Netgroup {  netgroup        = netgroup_name entry,
-                    description     = netgroup_description entry,
-                    netgroupTriples = netgroup_triples entry,
-                    memberNetgroups = netgroup_members entry
-                 }
+netgroup_members entry ngmap = []
 
+netgroup_from_map ngname ngmapio =
+    netgroupFromLDAP ngmapio (fromJust $ Map.lookup ngname ngmapio)
+
+lookupNetgroupMap :: String -> IO (Map.Map String LDAPEntry) 
+                            -> IO (Maybe LDAPEntry)
+lookupNetgroupMap ngname ngmap =
+                fmap (Map.lookup ngname) ngmap
+
+lookupNetgroupMapYeah :: String -> IO (Map.Map String LDAPEntry)
+                                -> IO LDAPEntry
+lookupNetgroupMapYeah ngname ngmap =
+                fmap fromJust (lookupNetgroupMap ngname ngmap)
+
+mapFromNetgroup :: (IO [LDAPEntry]) -> (IO (Map.Map String LDAPEntry))
+mapFromNetgroup entries =
+    fmap Map.fromList (fmap (map ( \ng -> (netgroup_name ng, ng))) entries)
+
+netgroupMap :: IO (Map.Map String LDAPEntry)
+netgroupMap = mapFromNetgroup searchNetgroupsInLDAP
+
+build_netgroups_from_ldap =
+    fmap (map $ netgroupFromLDAP []) searchNetgroupsInLDAP
+
+{-
 build_netgroups_from_ldap = do
-    fmap (map netgroupFromLDAP) searchNetgroupsInLDAP
+    fmap (map netgroupFromLDAP ngmap) entries
+    where
+        entries = searchNetgroupsInLDAP
+        ngmap = fmap mapFromNetgroup entries
+-}
