630ad97459442f601e1ac27f5bde0f757a0c6469
Wil Cooley <wcooley@pdx.edu>
2011-03-08 10:35:07 -0800

    Many additions and changes to LDAP.Netgroup
    * Punt on making netgroup_members a list of Netgroups; instead, it will
      just be a list of names of Netgroups. I found I would have needed to
      make them type 'IO [Netgroup]' because they would come from LDAP as
      such.
    
    * Renamed to indicate that these return LDAP nisNetgroup objects ("LDAP
      netgroup") and not data type Netgroups.
        lookupNetgroupMap* to lookupLDAPNegroup*
        mapFromNetgroup to ldapNetgroupMapFromLDAP*
        netgroupMap to ldapNetgroupMap
        ngmap to lngmap
    
    * Added netgroupEdgesByMember, netgroupEdges, netgroupGraph and genGraph
      for generating GraphViz output.

--- hs/LDAP/Netgroup.hs
+++ hs/LDAP/Netgroup.hs
@@ -9,6 +9,7 @@ import Data.Maybe
 import LDAP hiding (description)
 import LDAP.Util
 import Netgroup
+import Text.Util
 
 -- Local data
 
@@ -24,21 +25,23 @@ search_ldap_netgroup lobj =
                         ldapSearch lobj
                         basedn
                         LdapScopeSubtree
-                        (Just "(&(objectclass=nisNetgroup)(cn=b*))")
+                        (Just "(&(objectclass=nisNetgroup)(cn=*))")
                         netgroup_attrs
 			False
-    
+
 
 searchNetgroupsInLDAP :: IO [LDAPEntry]
 searchNetgroupsInLDAP = do
                     ldapAnonSetup >>= search_ldap_netgroup
 
--- netgroupFromLDAP :: IO (Map.Map String LDAPEntry) -> IO Netgroup
-netgroupFromLDAP ngmap entry =
-        Netgroup {  netgroup        = netgroup_name entry,
+-- netgroupFromLDAP :: IO (Map.Map String LDAPEntry) -> IO LDAPEntry -> IO Netgroup
+-- netgroupFromLDAP lngmap entry =
+netgroupFromLDAP entry =
+            Netgroup {
+                    netgroup        = netgroup_name entry,
                     description     = netgroup_description entry,
                     netgroupTriples = netgroup_triples entry,
-                    memberNetgroups = netgroup_members entry ngmap
+                    memberNetgroups = netgroup_members entry
                  }
 
 -- Attribute extractors to go along with Netgroup records
@@ -49,40 +52,81 @@ netgroup_name      = ldapAttr1Yeah "cn"
 -- installed schema??
 netgroup_description = ldapAttr1 "description"
 netgroup_triples = ldapAttrYeah "nisNetgroupTriple"
+netgroup_members = ldapAttrYeah "memberNisNetgroup"
 
-netgroup_members_ entry ngmap = mem_entries
-        where   mem_names = ldapAttrYeah "memberNisNetgroup" entry
-                mem_entries = map ( \n -> netgroup_from_map n ngmap ) mem_names
 
-netgroup_members entry ngmap = []
+-- netgroup_members entry lngmap = namesToNetgroups (ldapAttrYeah "memberNisNetgroup" entry) lngmap
 
-netgroup_from_map ngname ngmapio =
-    netgroupFromLDAP ngmapio (fromJust $ Map.lookup ngname ngmapio)
+-- Given a list of netgroup names, produce a list of Netgroups. Note we also
+-- filter for Nothing, so the resulting list may be shorter
+{-
+namesToNetgroups :: [String] -> IO (Map.Map String LDAPEntry) -> IO [Netgroup]
+namesToNetgroups names lngmap =
+    sequence [ do { x <- netgroupFromMap n lngmap; return x}  | n <- names ]
+-}
 
-lookupNetgroupMap :: String -> IO (Map.Map String LDAPEntry) 
+
+-- Produce a Netgroup from an ldapNetgroupMap
+-- Note that we must already be certain the entry exists in the
+-- ldapNetgroupMap, otherwise we'll get an error
+{-
+netgroupFromMap :: String -> IO (Map.Map String LDAPEntry) -> IO Netgroup
+netgroupFromMap ngname lngmap = ng
+                where
+                    ldapnetgroup = lookupLDAPNetgroupYeah ngname lngmap
+                    ng = netgroupFromLDAP lngmap ldapnetgroup
+-}
+
+
+-- Look up an ldapNetgroup by name in the ldapNetgroup map
+lookupLDAPNetgroup :: String -> IO (Map.Map String LDAPEntry)
                             -> IO (Maybe LDAPEntry)
-lookupNetgroupMap ngname ngmap =
-                fmap (Map.lookup ngname) ngmap
+lookupLDAPNetgroup ngname lngmap =
+                do  m <- lngmap
+                    return $ Map.lookup ngname m
 
-lookupNetgroupMapYeah :: String -> IO (Map.Map String LDAPEntry)
-                                -> IO LDAPEntry
-lookupNetgroupMapYeah ngname ngmap =
-                fmap fromJust (lookupNetgroupMap ngname ngmap)
+lookupLDAPNetgroupYeah :: String -> IO (Map.Map String LDAPEntry)
+                                 -> IO LDAPEntry
+lookupLDAPNetgroupYeah ngname lngmap =
+                do  e <- lookupLDAPNetgroup ngname lngmap
+                    return $ fromJust e
 
-mapFromNetgroup :: IO [LDAPEntry] -> IO (Map.Map String LDAPEntry)
-mapFromNetgroup entries =
-    fmap Map.fromList (fmap (map ( \ng -> (netgroup_name ng, ng))) entries)
+ldapNetgroupMapFromLDAP :: IO [LDAPEntry] -> IO (Map.Map String LDAPEntry)
+ldapNetgroupMapFromLDAP entries =
+        do  es <- entries
+            return $ Map.fromList [ (netgroup_name lng, lng) | lng <- es ]
 
-netgroupMap :: IO (Map.Map String LDAPEntry)
-netgroupMap = mapFromNetgroup searchNetgroupsInLDAP
+ldapNetgroupMap :: IO (Map.Map String LDAPEntry)
+ldapNetgroupMap = ldapNetgroupMapFromLDAP searchNetgroupsInLDAP
 
 build_netgroups_from_ldap =
-    fmap (map $ netgroupFromLDAP []) searchNetgroupsInLDAP
+    do  m <- ldapNetgroupMap
+        return $ map netgroupFromLDAP (Map.elems m)
 
+netgroupEdgesByMember :: Netgroup -> [String]
+netgroupEdgesByMember ng = [ " " `x` 12 ++ ngname
+                            ++ " -> " ++ (map (subst '-' '_') m) ++ ";"
+                            | m <- members ]
+    where   ngname = map (subst '-' '_') $ netgroup ng
+            members = memberNetgroups ng
+
+subst x y z = if z /= x then z else y
+
+netgroupEdges :: [Netgroup] -> [String]
+netgroupEdges ngs = concat $ map netgroupEdgesByMember ngs
+
+netgroupGraph =
+    do  ngs <- build_netgroups_from_ldap
+        return (unlines $ netgroupEdges ngs)
+
+genGraph =
+    do  putStrLn "digraph netgroups {"
+        netgroupGraph >>= putStr
+        putStrLn "}"
 {-
 build_netgroups_from_ldap = do
-    fmap (map netgroupFromLDAP ngmap) entries
+    fmap (map netgroupFromLDAP lngmap) entries
     where
         entries = searchNetgroupsInLDAP
-        ngmap = fmap mapFromNetgroup entries
+        lngmap = fmap ldapNetgroupMapFromLDAP entries
 -}
